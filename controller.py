70ecf0bb04ebac1fc157c2b2a0fcef2a88290817
Marek <mdaw@poczta.fm>
2023-08-09 18:10:09 +0200

    adjust prepare_weather_data to update_gui_from_api in GUI file.

--- controller.py
+++ controller.py
@@ -6,21 +6,27 @@ class CurrentWeatherController:
         self.model = model
 
     def prepare_weather_data(self):
-        city = ''
-        temperature = ''
-        pressure = ''
+        city = ""
+        temperature = ""
+        pressure = ""
+        humidity = ""
+        wind = ""
 
         data = self.model.get_api_data()
 
         for key, value in data.items():
-            if key == 'stacja':
+            if key == "stacja":
                 city = value
-            elif key == 'temperatura':
+            elif key == "temperatura":
                 temperature = value
-            elif key == 'cisnienie':
+            elif key == "cisnienie":
                 pressure = value
+            elif key == "predkosc_wiatru":
+                wind = value
+            elif key == "wilgotnosc_wzgledna":
+                humidity = value
 
-        return city, temperature, pressure
+        return city, temperature, pressure, humidity, wind
 
     def prepare_data(self):
         location, temperature, pressure, = self.prepare_weather_data()
