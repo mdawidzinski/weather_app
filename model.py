81cb4d94df68cf0270459ea39ab84ff9f330526e
Marek <mdaw@poczta.fm>
2023-08-21 20:53:51 +0200

    Created prepare_data for preparing data for the graph. Added comments.

--- model.py
+++ model.py
@@ -6,25 +6,30 @@ class CurrentWeatherModel:
     def __init__(self, data):
         self.url = "https://danepubliczne.imgw.pl/api/data/synop/station/wroclaw"
         self.data = data
+        self.graph = ""
+        self.data_type = ""
+        self.period = ""
 
     def get_api_data(self):
+        """Method responsible for retrieving data from API"""
         response = requests.get(self.url)
         api_data = response.json()
         return api_data
 
     def get_excel_data(self):
+        """Method responsible for reading data from Excel"""
         weather_data = pd.read_excel(self.data)
         return weather_data
 
-    def get_avg_temp(self):
+    def prepare_data(self):
+        """Method responsible for preparing data for the graph"""
         weather_data = self.get_excel_data()
 
-        target_date = pd.Timestamp(year=pd.Timestamp.today().year, month=pd.Timestamp.today().month,
-                                   day=pd.Timestamp.today().day)
+        last_row = weather_data.iloc[-1]
+        last_day = last_row["date"]
 
-        df_previous_years = weather_data[(weather_data['date'].dt.month == target_date.month) &
-                                         (weather_data['date'].dt.day == target_date.day) &
-                                         (weather_data['date'].dt.year < target_date.year)]
+        first_day_of_last_month = last_day.replace(day=1)
 
-        avg_temp = df_previous_years['tavg'].mean()
-        return avg_temp
+        filtered_data = weather_data[weather_data['date'].between(first_day_of_last_month, last_day)]
+
+        return filtered_data, self.data_type
